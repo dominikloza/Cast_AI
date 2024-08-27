a2afa2fc6389423294b7af3a3a31dca516c80112
dominikloza <dominikloza110899@gmail.com>
2024-08-28 00:27:06 +0200

    Creating structure of folders

---
+++ cast_ai/components/LeftSidebar.tsx
@@ -0,0 +1,24 @@
+import { sidebarLinks } from '@/constants'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const LeftSidebar = () => {
+    return (
+        <section className='left_sidebar'>
+            <nav className=' flex flex-col gap-6'>
+                <Link href="/" className='flex cursor-pointer items-center gap-2 pb-10 max-lg:justify-center'>
+                    <Image src="/icons/logo.svg" alt='logo' width={23} height={27} />
+                    <h1 className='text-24 font-extrabold text-white max-lg:hidden'>Cast AI</h1>
+                </Link>
+                {sidebarLinks.map(({route, label}) => {
+                    return <Link href={route}>
+                        {label}
+                    </Link>
+                })}
+            </nav>
+        </section>
+    )
+}
+
+export default LeftSidebar

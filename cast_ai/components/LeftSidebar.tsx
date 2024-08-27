18acdc09feb9ea9339efdb42580c4b9a1a66da9b
dominikloza <dominikloza110899@gmail.com>
2024-08-28 01:13:49 +0200

    Making homepage layout

--- cast_ai/components/LeftSidebar.tsx
+++ cast_ai/components/LeftSidebar.tsx
@@ -1,9 +1,15 @@
+'use client';
+
 import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname, useRouter } from 'next/navigation'
 import React from 'react'
 
 const LeftSidebar = () => {
+    const pathname = usePathname();
+    const router = useRouter();
     return (
         <section className='left_sidebar'>
             <nav className=' flex flex-col gap-6'>
@@ -11,8 +17,12 @@ const LeftSidebar = () => {
                     <Image src="/icons/logo.svg" alt='logo' width={23} height={27} />
                     <h1 className='text-24 font-extrabold text-white max-lg:hidden'>Cast AI</h1>
                 </Link>
-                {sidebarLinks.map(({route, label}) => {
-                    return <Link href={route}>
+                {sidebarLinks.map(({ route, label, imgURL }) => {
+                    const isActive = pathname === route || pathname.startsWith(`${route}/`);
+                    return <Link href={route} key={label} className={cn('flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start transition-all', {
+                        'bg-nav-focus border-r-4 border-orange-1': isActive
+                    })}>
+                        <Image src={imgURL} alt={label} width={24} height={24} />
                         {label}
                     </Link>
                 })}

2439066714b7a297e7e8ce06dd415bcb72cd6cc5
dominikloza <dominikloza110899@gmail.com>
2024-08-28 15:26:58 +0200

    Adding auth, fixing structure of app

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+const authConfig = {
+    providers: [
+        {
+            domain: "https://adapted-duck-44.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
+
+export default authConfig;
